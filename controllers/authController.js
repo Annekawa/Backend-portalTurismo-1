2a80cd9ff9a05d99c82b44a12f2bcc99b7e09454
Annekawa <annekawa77@gmail.com>
2025-07-29 16:04:27 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/users');
+const user = require('../models/users');
  
-exports.login = async(req, res) => {
+exports.login = async (req, res) => {
     try {
-        const {email, password} = req.body;
-        const user = await User.findOne({where: {email}})
+        const{email, password} = req.body;
+        const user =await User.findOne({where: {email}})
         if(!user) return res.status(404).json({message: 'Usuário não encontrado'})
-        const passwordValid = await bcrypt.compare(password, user.password)
-        if(!passwordValid) return res.status(400).json({message: 'email ou senha estão incorretos'})
+        const passwordValid =await bcrypt.compare(password, user.password)
+        if(!passwordValid)return res.status(400).json({message: 'email ou senha estão incorretos'})
         res.json({message: 'Login realizado com sucesso',
             user: {id: user.id, name: user.name, email: user.email}
             })
